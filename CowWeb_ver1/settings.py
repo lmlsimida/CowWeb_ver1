0126b5794753202d70e0270803d86ecc01f431a1
haiyang <yang.hai@yiconmed.com>
2023-10-20 14:40:35 +0800

    修改STATIC_URL

--- CowWeb_ver1/settings.py
+++ CowWeb_ver1/settings.py
@@ -126,7 +126,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "backend-static/"
 STATIC_ROOT = "static"
 # 文件目录
 MEDIA_URL = "/media/"
