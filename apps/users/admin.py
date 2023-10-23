0e381aa64f985b4c209bdfc88e30b48c5fe70481
haiyang <yang.hai@yiconmed.com>
2023-10-23 18:01:55 +0800

    新功能

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,31 +1,34 @@
-from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import AnonymousUser
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from apps.users.models import User
 
 
-class AdminUserFrom(forms.ModelForm):
+class AdminUserForm(UserCreationForm):
     """
     superuser的用户表单
     """
 
-    def clean_factory(self):
+    def clean_pasture(self):
         """
         当用户不为超级用户时，工厂必填
         """
         is_superuser = self.cleaned_data.get("is_superuser")
-        factory = self.cleaned_data.get("factory")
-        if not is_superuser and not factory:
-            raise ValidationError("如果用户不是超级用户，工厂字段是必填的。")
-        return factory
+        pasture = self.cleaned_data.get("pasture")
+        if not is_superuser and not pasture:
+            raise ValidationError("如果用户不是超级用户，牧场字段是必填的。")
+        return pasture
 
     class Meta:
         model = User
         fields = "__all__"
 
 
-class CustomUserForm(forms.ModelForm):
+class CustomUserForm(UserCreationForm):
     """
     自定义用户表单用户一级用户创建新用户
     """
@@ -36,7 +39,28 @@ class CustomUserForm(forms.ModelForm):
 
 
 @admin.register(User)
-class UserModelAdmin(admin.ModelAdmin):
+class UserModelAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "email", "level", "pasture")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
     def has_add_permission(self, request):
         user = request.user
         return user.is_superuser or user.level == 1
@@ -56,24 +80,34 @@ class UserModelAdmin(admin.ModelAdmin):
             return qs
 
         if request.user.level == 1:  # 一级用户
-            return qs.filter(factory=request.user.factory)
+            return qs.filter(pasture=request.user.pasture)
 
         return qs.none()  # 其他用户不显示任何数据
 
     def save_model(self, request, obj, form, change):
         if not (request.user.is_superuser and obj.is_superuser) and obj.level != 1:
-            obj.factory = request.user.factory
+            obj.pasture = request.user.pasture
         obj.is_staff = True
         super().save_model(request, obj, form, change)
 
     def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during user creation
+        """
+        defaults = {}
         user = request.user
-        if user.is_superuser:
-            return AdminUserFrom
-        if user.level == 1:
-            return CustomUserForm
-        return super().get_form(request, obj, **kwargs)
+        if obj is None:
+            if user.is_superuser:
+                self.add_form = AdminUserForm
+            elif user.level == 1:
+                self.add_form = CustomUserForm
+            defaults["form"] = self.add_form
+
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
 
     def has_module_permission(self, request):
         user = request.user
+        if isinstance(user, AnonymousUser):
+            return False
         return user.is_superuser or user.level == 1
